296d8037abbacfc25c2065586c84c68059838986
MATBMS <mathieu.bosmans@outlook.com>
2026-01-31 19:10:10 +0100

    Add interactivity closes #3

---
+++ scripts/submit.js
@@ -0,0 +1,36 @@
+// Define UI components
+const cardForm = document.getElementById('submit-card');
+const cardName = document.getElementById('card-name');
+const cardNumber = document.getElementById('card-number');
+const cardExpMonth = document.getElementById('card-exp-month');
+const cardExpYear = document.getElementById('card-exp-year');
+const cardCvc = document.getElementById('card-cvc');
+
+// Define card info
+const cardInfo = {
+  name: 'Jane Appleseed',
+  number: 123456789123,
+  expMonth: 1,
+  expYear: 1,
+  Cvc: 123,
+};
+
+// Submit form
+function submitForm(e) {
+  // Prevent default behavior
+  e.preventDefault();
+
+  // Submit value
+  cardInfo.name = cardName.value;
+  cardInfo.number = cardNumber.value;
+  cardInfo.expMonth = cardExpMonth.value;
+  cardInfo.expYear = cardExpYear.value;
+  cardInfo.Cvc = cardCvc.value;
+
+  console.log(cardInfo);
+
+  // Redirect
+  window.location.href = 'thank-you.html';
+}
+
+cardForm.addEventListener('submit', submitForm);
